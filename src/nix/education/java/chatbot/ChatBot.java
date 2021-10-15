4c3d9a6d0568c25673b06c7e287a0f0046bb9961
0eagermax0 <m.liubchenko@student.csn.khai.edu>
2021-10-15 14:06:16 +0300

    ChatBot 2-nd stage

--- src/nix/education/java/chatbot/ChatBot.java
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -1,2 +1,12 @@
-package nix.education.java.chatbot;public class ChatBot {
+package nix.education.java.chatbot;
+
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args){
+    System.out.println("Hello! My name is MaxoBot.\nI was created in 2021.\nPlease, remind me your name.");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
+    }
 }
